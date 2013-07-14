f42401f0cd7abdcadddb0f5d0029f031ec54e1ce
Alexander Schier <allo@laxu.de>
2013-07-14 20:51:41 +0200

    added MessageDialog to ShareIntent

--- src/de/laxu/apps/sharedurllist/ShareURLIntent.java
+++ src/de/laxu/apps/sharedurllist/ShareURLIntent.java
@@ -1,22 +1,48 @@
 package de.laxu.apps.sharedurllist;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class ShareURLIntent extends Activity {
+public class ShareURLIntent extends FragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		finish();
-		Intent intent = getIntent();
+		Intent intent = this.getIntent();
 		String action = intent.getAction();
 		String type = intent.getType();
 		if(Intent.ACTION_SEND.equals(action) && type != null && type.equals("text/plain")){
 			String url = intent.getStringExtra(Intent.EXTRA_TEXT);
-			
+			(new MessageDialogFragment()).setUrl(url). show(getFragmentManager(), "successDialog");
+		}
+		//finish();
+	}
+	public static class MessageDialogFragment extends DialogFragment{
+		private String url;
+		public MessageDialogFragment setUrl(String url){
+			this.url = url;
+			return this;
+		}
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState){
+			AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
+			builder
+			.setTitle("added URL to URL-List")
+			.setMessage("added URL: "+url)
+			.setIcon(android.R.drawable.ic_dialog_info)
+			.setPositiveButton("OK", new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					getActivity().finish();
+				}
+			});
+			return builder.create();
 		}
 	}
-
 }
